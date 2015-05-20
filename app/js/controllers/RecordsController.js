12ea6b5d84eeacc53e8b8c50027bfd4ccb7a5db1
soka <kobvel@gmail.com>
2015-05-20 16:09:39 +0300

    iframe and pdf from html init

--- app/js/controllers/RecordsController.js
+++ app/js/controllers/RecordsController.js
@@ -4,16 +4,35 @@
     .module('patientrec.controllers')
     .controller('RecordsController', RecordsController);
 
-  RecordsController.$inject = ['RecordsDB', 'records', '$scope', '$filter'];
+  RecordsController.$inject = ['RecordsDB', 'records', '$scope', '$filter', '$sce'];
 
-  function RecordsController(RecordsDB, records, $scope, $filter) {
+  function RecordsController(RecordsDB, records, $scope, $filter, $sce) {
     var self = this;
-
+    self.pdfSrc = null;
     self.firstForm = {};
 
     self.records = records;
     self.inspectSelected = 'шлунок';
+    self.filterTable = null;
+    self.filter = {
+      department: '',
+      doctor: '',
+      inspections: '',
+      type: '',
+      date: ''
+    }
+    self.filters = function() {
+      console.log(records);
+      self.filter = {
+        department: '',
+        doctor: '',
+        inspections: '',
+        type: '',
+        date: ''
+      }
+      console.log(records);
 
+    }
     var newRecord = {
       name: '',
       surname: '',
@@ -40,6 +59,25 @@
       }
     };
     self.newRecord = angular.copy(newRecord);
+    self.trustSrc = function(src) {
+      return $sce.trustAsResourceUrl(src);
+    }
+    self.generatePdf = function() {
+      var pdf = new jsPDF('p', 'pt', 'a4');
+      pdf.text(20, 20, 'Hello world!');
+      pdf.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+      pdf.addPage('a6', 'l');
+      pdf.text(20, 20, 'Do you like that?');
+
+
+      var string = pdf.output('datauristring');
+      console.log(string);
+      $('.preview-pane').attr('src', string);
+
+
+
+
+    }
     $scope.today = function() {
       self.newRecord.date = new Date();
     };
@@ -107,4 +145,6 @@
       'Киценко Віталій Олегович'
     ];
   }
+
+
 })();
